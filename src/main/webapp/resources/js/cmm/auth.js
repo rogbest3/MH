977b701e62bdce134d0a4cb70c5addd63ba62bc4
gkry21 <gkry21@naver.com>
2019-11-15 15:20:27 +0900

    Apply .gitignore

--- src/main/webapp/resources/js/cmm/auth.js
+++ src/main/webapp/resources/js/cmm/auth.js
@@ -1,24 +1,73 @@
 "use strict";
 var auth = auth || {};
 auth =(()=>{
-	const WHEN_ERR = 'auth 호출하는 JS 파일을 찾지 못했습니다.'
-   let _, js, vue,css,img,crowl_js;
+	const WHEN_ERR = 'auth호출하는 JS 파일을 찾지 못했습니다.'
+   let _, js, vue,css,img,brd_js, cookie_js,crowler_js;
    let init =()=> {
 	   _ = $.ctx()
 	   js = $.js()
 	   css = $.css()
 	   img = $.img()
-       vue = js + '/vue/auth_vue.js'  //메인 화면 구성 크롤러 클릭 -> 웹크롤링 선택 3개
-       crowl_js = js + '/crowler/crowler.js'
+       vue = js + '/vue/auth_vue.js'
+       cookie_js = js+'/cmm/cookie.js'
+       crowler_js = js+'/crowler/crowler.js'
+       
    }
-   let onCreate =()=>{
-	   init();
-	   $.when($.getScript(vue)
-			   ).done(()=>{
-//				   setContentView()
-			   }).fail(()=>{
-				   alert(WHEN_ERR)
-			   })
-   }
-   return {onCreate}
-})
+   let onCreate =()=> {
+       init();
+       $.when($.getScript(vue),
+    		   $.getScript(crowler_js)
+    		   )
+       .done(()=>{
+    	   setContentView()
+       }).fail(()=>{alert(WHEN_ERR)})
+       }
+
+       let setContentView =()=>{
+    	   $('head').html(auth_vue.login_head({css: $.css(), img: $.img(),  js: $.js()}))
+           $('body').addClass('text-center')
+           .html(auth_vue.login_body({css: $.css(), img: $.img(),  js: $.js()}))
+   		login()
+   		access()
+       }
+
+      let login =()=>{
+          let x =  {css: $.css(), img: $.img(), js :$.js()}
+          $('head').html(auth_vue.login_head(x))
+          $('body').addClass('text-center')
+          .html(auth_vue.login_body(x))
+          $('<button>',{
+   			text:"Sign in",
+   			type:"submit",
+//   			click: e =>{
+//   				e.preventDefault();
+//               let data = {cemail : $('#cemail').val(), cpwd : $('#cpwd').val()}
+//               alert(data.cemail)
+//                	$.ajax({
+//                		url : _+'/user/' + data.cemail+'/login',
+//                		type : 'POST',
+//                		dataType : 'json',
+//                		data : JSON.stringify(data),
+//                		contentType : 'application/json',
+//                		success : d => {
+//                			setCookie("CEMAIL",d.cemail)
+//                			alert('저장된 쿠키:'+getCookie("CEMAIL"))
+//                				brd.onCreate()
+//                			},
+//                			 error : e => {
+//                                 alert('AJAX 실패');
+//                			 }
+//                       })
+//                      
+//                       }
+                	}).addClass("btn btn-lg btn-primary btn-block")
+		.appendTo('#btn_login')
+      }
+
+      let access =()=>{
+    	  $('#a_go_admin').click(e=>{
+    		  crowler.onCreate()
+    	  })
+      }
+   return{onCreate,login}
+})();
--- src/main/webapp/resources/js/crowler/crowler.js
+++ src/main/webapp/resources/js/crowler/crowler.js
@@ -1,3 +1,127 @@
-/**
- * 
- */
+"use strict"
+var crowler = crowler ||{}
+crowler = (()=>{
+	const WHEN_ERR = 'crowler호출하는 js를 찾을 수 없습니다'
+	let _ , js , img , css , navi_vue_js;
+	let init =()=>{
+		_=$.ctx()
+		js=$.js()
+		img=$.img()
+		css=$.css()
+		navi_vue_js = js+'/vue/navi_vue.js'
+	}
+	let onCreate =()=>{
+		init()
+		$.when(
+			$.getScript(navi_vue_js)
+			)
+		.done(()=>{
+			setContentView()
+		})
+		.fail(()=>{WHEN_ERR})
+	}
+	
+	let setContentView=()=>{
+		$('body').empty()
+		.css({'background-color': '#be7a7a'})
+		$(navi_vue.nav())
+		.appendTo('body')
+		$('<table id="tab">'+
+				'<tr>'+
+				'</tr>'+
+		'</table>')
+		.css({border:'1px solid black',width: '100%',height:'80%', 'margin':'0 auto'})
+		.appendTo('body')
+		$.each([{id :'left',widgh: '20%'},
+			{id : 'right',widgh:'80%'}],
+			(i,j)=>{
+			$('<td id="'+j.id+'"></td>')
+			.css({border: '2px solid #ddd',width: j.widgh,'vertical-align': 'top'})
+			.appendTo('#tab tr')
+		})
+
+		$.each([
+			 {txt:'웹크롤링',name: 'web_crawl'},
+			 {txt:'고객관리',name: 'cust_mgmt'},
+			 {txt:'커뮤니티관리',name: 'comm_mgmt'}],
+			(i,j)=>{
+			$('<div name="'+j.name+'">'+j.txt+'</div>')
+			.css({border: '1px solid #ddd', margin: '0 auto ', 'line-height': '50px'})
+			.appendTo('#left')
+			.click(function(){
+				$(this).addClass('active')
+				$(this).siblings().removeClass('active')
+				switch($(this).attr('name')){
+				case 'web_crawl': webCrawl()
+				break;
+				case 'cust_mgmt': cust_mgmt()
+					break;
+				case 'comm_mgmt': comm_mgmt()
+					break;
+				}
+		})
+	})
+}
+	let webCrawl=()=>{
+        $('#right').empty()
+        $('</br></br></br></br></br><h2>Web Crawling</h2></br></br></br></br></br></br></br>'+
+                '<form id="crawl_form" class="form-inline my-2 my-lg-0">'+
+                '  <select name="site" size="2" multiple>'+
+                '  </select>'+
+                '<input class="form-control mr-sm-2" value="exratecheck" type="text" placeholder="insert URL for crawling" aria-label="Search">'+
+                '</form>')
+        .appendTo('#right')
+        $('#crawl_form input[class="form-control mr-sm-2"]')
+        .css({width:'80%'})
+        $.each([{sub:'직접 입력'},{sub:'naver.com'},{sub:'daum.net'},{sub:'google.co.kr'},{sub:'youtube.com'}],(i,j)=>{
+            $('<option value='+j.sub+'>'+j.sub+'</option>').appendTo('#crawl_form select')
+        })
+        $('<button class="btn btn-secondary my-2 my-sm-0" type="submit">go crawl</button>')
+        .appendTo('#crawl_form')
+        .click(e=>{
+            alert(
+            $.fn.nullChecker([$('#crawl_form select[name="site"]').val(),
+                    $('#crawl_form input[type="text"]').val()])?'빈칸을 채우시오':'보낼준비 완료')
+            let url = _+'/tx/crawling/'+$('#crawl_form select[name="site"]').val()
+            +'/'+$('#crawl_form input[type="text"]').val()
+            alert($('#crawl_form select[name="site"]').val())
+            e.preventDefault() // /tx/crawling/google/spring
+            $.getJSON(_
+                    +'/tx/crawling/'+$('#crawl_form select[name="site"]').val()
+                    +'/'+$('form#crawl_form input[type="text"]').val(),
+                    d=>{
+                alert(d.msg)
+            })
+        })
+    }
+	let cust_mgmt=()=>{
+	     $('#right').empty()		
+		$('<a>고객 테이블 생성</a></br>')
+		.appendTo('#right')
+		.click(e=>{
+			e.preventDefault()
+			$.getJSON(_+'/user/create/table',d=>{
+				alert('테이블 생성 성공여부 :'+d.msg)
+			})
+		})
+		$('<a>고객테이블 삭제</a></br>')
+		.appendTo('#right')
+		.click(e=>{
+			e.preventDefault()
+			$.getJSON(_+'/user/drop/table',d=>{
+				alert('일괄등록된 유저의 수 :'+d.userCount)
+			})
+		})
+		$('<a>고객명단 대량 생성</a></br>')
+		.appendTo('#right')
+		.click(e=>{
+			e.preventDefault()
+			$.getJSON(_+'/tx/register/users',d=>{
+				alert('일괄등록된 유저의 수 :'+d.userCount)
+			})
+		})
+	}
+
+
+	return {onCreate}
+})();
